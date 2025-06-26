aaf207a4e680b228764658e9d49354f08a8a87f1
Dibbyo1 <dibbyosaha16@gmail.com>
2025-06-26 13:27:12 -0400

    Create AssemblyInfo.cs

---
+++ Banking System/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Banking System")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Banking System")]
+[assembly: AssemblyCopyright("Copyright ©  Dibbyo Saha 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+
+[assembly: Guid("4f0c7ab0-374f-4411-9f63-a88171fc2e3a")]
+
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
